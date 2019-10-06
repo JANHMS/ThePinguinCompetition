bb75eab046bc704d7675f40c8751f435a3814a4e
Janneman <Jan@kooperativ.net>
2019-10-06 19:24:44 +0200

    First Commit with code

---
+++ js/app.js
@@ -0,0 +1,29 @@
+//set a model
+//Javascript try to implement small project Jan
+var model = {
+    currentpinguin: null,
+    pinguins: [
+        {
+            clickCount : 0,
+            name : 'Cutie1',
+            imgSrc : 'img/pinguin1.jpg',
+
+        },
+        {
+            clickCount : 0,
+            name : 'Cutie2',
+            imgSrc : 'img/Pinguin2.jpg',
+        },
+        {
+            clickCount : 0,
+            name : 'Cutie3',
+            imgSrc : 'img/pinguin3.jpg',
+        },
+        {
+            clickCount : 0,
+            name : 'Cutie4',
+            imgSrc : 'img/pinguin4.jpg',
+        },
+
+    ]
+};
